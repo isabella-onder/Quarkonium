62aff69a50630cbc79d896024bebab7d7c2d4005
Isabella Onder <bella.castelbranco@gmail.com>
2025-10-14 17:25:52 +0100

    Succesfully plots u(n) when given all exact parameters for Hydrogen

--- milestone.py
+++ milestone.py
@@ -1,28 +1,47 @@
+#%%
+
+
+
 import scipy as sp
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+#important to note: using h_bar = c = 1
 
 def system(r, Y, l, mu, E_nl, alpha, beta): #defining my system of differential equations according to all variables
     u, v = Y                                #unpacking y 
     du_dr = v                               
-    dv_dr = l*(l+1)/r**2 * u - 2 * mu(E_nl - [-(4*alpha)/(3*r) + beta*r]) * u  #hmm what is beta and how do I find it
+    dv_dr = (l*(l+1))/(r**2) * u - (2 * mu * (E_nl - ((-alpha)/(r) + beta*r))) * u  #hmm what is beta and how do I find it + removed 4/3 for Hydrogen testing
     return [du_dr, dv_dr] 
 
 
 
-def sch_solver(l,m_1,m_2, E_nl, alpha, beta): #for now pass everything as arguments
+def sch_solver(l,m_1,m_2, n, alpha, beta): #for now pass everything as arguments
     mu = (1/m_1 + 1/m_2) ** (-1)
-    initial_conditions = [0,1] #because we want v(0) = 1 and u(0) = 0 
+    E_nl = - mu * alpha**2/(2*n**2)
+    print(E_nl)
+    #print('this is mu', mu)
+    initial_conditions = [0,1] #because we want u(0) = 0, du(0)/dr = v(0) = 1
 
-    r, Y = sp.integrate.solve_ivp(system, [0,15], initial_conditions, args = (l, mu, E_nl, alpha, beta)) #need to find a way to put r in GeV as in get all the units right
-    u, v = Y
+    a0 = 1/(mu*alpha)  # Bohr radius in GeV^-1
+    r0 = 1e-6 * a0     # small start
+    rmax = 20 * a0      # extend beyond peak
+
+    print ('this is a_o', a0)
+    t_eval = np.linspace(r0,rmax,1510)
+    print(t_eval)
+
+    sol = sp.integrate.solve_ivp(system, [r0,rmax], initial_conditions, t_eval = t_eval, args = (l, mu, E_nl, alpha, beta)) #need to find a way to put r in GeV as in get all the units right
+    u, v = sol.y[0], sol.y[1]
+    r = sol.t
+    print(r)
 
     plt.scatter(r,u)
     plt.show()
 
 
-
-    #
-
+print(sch_solver(1,0.000511,100000000000,2,1/137,0))
     
 
+
+# %%
--- test.py
+++ test.py
@@ -1,4 +1,33 @@
 #this is my test file
 #this is me trying a commit 
+print('Hello, World!')
+#this is me trying to change stuff on a branch
 
-#and this is me trying to make conflicts
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+#testing by doing a simple spring SHM system
+
+def system(t, Y, w ): #defining my system of differential equations according to all variables
+    y1, y2 = Y                                #unpacking y 
+    dy1_dt = y2                             
+    dy2_dt = - w**2 * y1
+    return [dy1_dt, dy2_dt] 
+
+
+
+def diff_solver(w): #for now pass everything as arguments
+    initial_conditions = [1,0] #because we want x(0) = o and dx/dt(0) = 1 
+
+    t_eval = np.linspace(0.1,15,151)
+
+    sol = sp.integrate.solve_ivp(system, [0.1,15], initial_conditions, t_eval = t_eval, args = (w,)) #need to find a way to put r in GeV as in get all the units right
+    x, v = sol.y[0], sol.y[1]
+    r = sol.t
+
+
+    plt.scatter(r,x)
+    plt.show()
+
+diff_solver(2)
